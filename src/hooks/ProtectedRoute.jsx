1b51a536a276d99af39487a209cc5d09d87e3e09
abfatahi <abfatahi.iaf@gmail.com>
2022-02-18 02:09:10 +0100

    Fix: protected route

--- src/hooks/ProtectedRoute.jsx
+++ src/hooks/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ import { Outlet, Navigate } from 'react-router-dom';
 const Index = ({ component: Component, ...rest }) => {
   let isAuthorized = false;
   const token = localStorage.getItem('token');
-  if (token !== null || undefined || '') {
+  if (token !== null) {
     isAuthorized = true;
   }
   return isAuthorized ? <Outlet /> : <Navigate to='/login' />;
